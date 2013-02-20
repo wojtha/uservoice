5ce349c15ee4264184f767c46548638ec19d6428
Vojtech Kusy <wojtha@gmail.com>
2013-02-20 13:25:59 +0100

    Make sure that sso is not empty in test_sso_token_set

--- test/single_sign_on_test.rb
+++ test/single_sign_on_test.rb
@@ -6,6 +6,6 @@ class SingleSignOnControllerTest < ActionController::TestCase
   def test_sso_token_set
     get :action_with_sso
     assert_match Regexp.new('<script type="text/javascript">.*var uvOptions = .*\s*</script>', Regexp::MULTILINE), @response.body
-    assert_match /"params":\{"sso":".*"\}/, @response.body
+    assert_match /"params":\{"sso":".+"\}/, @response.body
   end
 end
